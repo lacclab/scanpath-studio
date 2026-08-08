d00a5dd7d600c12fbb380d554592a253ba3502d3
Omer Shubi <omer.shubi@gmail.com>
2026-08-08 13:12:13 +0300

    Release v0.28.0

--- scanpath_studio/__init__.py
+++ scanpath_studio/__init__.py
@@ -27,7 +27,7 @@ __all__ = [
     "load_multipleye",
     "load_onestop",
 ]
-__version__ = "0.27.2"
+__version__ = "0.28.0"
 
 # Public headless API (see api.py / datasets.py). Resolved lazily so
 # `import scanpath_studio` stays cheap and doesn't pull in pandas/plotly/
